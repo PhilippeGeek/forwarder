dd4ef3a8bcf1f006d78512af0f44096550fd26c9
Philippe Vienne <philippegeek@gmail.com>
2016-08-20 10:24:34 +0200

    Use a 301 (easier to manage)

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer();
 
 const server = http.createServer(function (req, res) {
-    proxy.web(req, res, { target: process.env.REDIRECTED_URL||'https://www.twin2green.com' })
+    res.writeHead(301,
+        {Location: (process.env.REDIRECTED_URL||'https://www.twin2green.com/admin')+req.url}
+    );
+    res.end();
 });
 
 server.listen(process.env.PORT||5000);
