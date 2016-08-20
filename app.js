e36fe01c6a1626971f0726d5859b69efdc6de05e
Philippe Vienne <philippegeek@gmail.com>
2016-08-20 09:48:26 +0200

    Add environement variables

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer();
 
 const server = http.createServer(function (req, res) {
-    proxy.web(req, res, { target: 'https://www.twin2green.com' })
+    proxy.web(req, res, { target: process.env.REDIRECTED_URL||'https://www.twin2green.com' })
 });
 
-server.listen(5000);
+server.listen(process.env.PORT||5000);
